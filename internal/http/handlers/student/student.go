6aff0edc6f6405348120ec494fb541921195eb07
Avinash Gupta <guptaavinash302@gmail.com>
2025-05-07 23:56:25 +0530

    modified student.go

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -3,16 +3,18 @@ package student
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/laladwesh/proj-tut/internal/storage"
 	"github.com/laladwesh/proj-tut/internal/types"
 	"github.com/laladwesh/proj-tut/internal/utils/response"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		var student types.Student
@@ -38,6 +40,18 @@ func New() http.HandlerFunc {
 			return
 		}
 
+		lastid, err := storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+
+		if err != nil {
+			response.WriteJSON(w, http.StatusInternalServerError, err)
+			return
+		}
+		slog.Info("user created successfully ", slog.String("userId", fmt.Sprint(lastid)))
+		response.WriteJSON(w, http.StatusCreated, map[string]int64{"id": lastid})
 		slog.Info("Creating a student", slog.String("method", r.Method), slog.String("path", r.URL.Path))
 		response.WriteJSON(w, http.StatusCreated, map[string]string{"message": "student created"})
 	}
